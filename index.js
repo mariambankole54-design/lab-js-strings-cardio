e4fc4b9f9b349cc481c07c209ee717bfdb21c5be
Mariam Bankole <mariambankole54@gmail.com>
2026-01-31 15:42:50 +0000

    lab

--- index.js
+++ index.js
@@ -5,9 +5,9 @@
 
 const string1 = "My favorite dessert is jello";
 
+
 // Your code here...
-
-
+console.log(string1.indexOf("j"));
 
 
 /*******************************************
@@ -18,6 +18,8 @@ const string1 = "My favorite dessert is jello";
 const string2 = "ABCDEFGHJKLO";
 
 // Your code here...
+const coolString = string2[2] + string2[11] + string2[11] + string2[10];
+console.log(coolString);
 
 
 
@@ -30,6 +32,7 @@ const string2 = "ABCDEFGHJKLO";
 const string3 = "Na";
 
 // Your code here...
+console.log(string3.repeat(4) + " Batman!");
 
 
 
@@ -42,6 +45,8 @@ const string3 = "Na";
 const fruit = "banana apple mango orange lemon kiwi watermelon grapes pear pineapple";
 
 // Your code here...
+const favoriteFruit = fruit.slice(13, 18); // Example: "mango"
+console.log(favoriteFruit);
 
 
 
@@ -58,11 +63,21 @@ const funnyHeadline2 = "Students Cook & Serve Grandparents";
 
 // Check the first headline
 // Your code here ...
-
+if (funnyHeadline1.includes("oxygen"))
+{
+    console.log("The string includes the word 'oxygen'");
+}
 
 // Check the second headline
 // Your code here ...
-
+if (funnyHeadline2.includes("oxygen"))
+{
+    console.log("The string includes the word 'oxygen'");
+}
+else
+{
+    console.log("The string does not include the word 'oxygen'");
+}
 
 
 /*******************************************
@@ -75,7 +90,9 @@ const string4 = "zEAWrTC9EgtxmK9w1";
 
 // a) Print the string length
 // Your code here ...
+console.log(string4.length);
 
 
 // b) Print the last character in the string
 // Your code here ...
+console.log(string4[string4.length - 1]);
